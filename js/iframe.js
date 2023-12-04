0da3f11e732a5d7bc0b17cbe04ceac49ae279248
NastyaBoiko <killer99whale@mail.ru>
2023-12-04 15:32:05 +0300

    change adoptive points

--- js/iframe.js
+++ js/iframe.js
@@ -3,9 +3,9 @@ console.log(iframe);
 // изменение iframe карты для экранов
 
 // Создаем медиа условие, проверяющее viewports на ширину менее 1440 пикселей.
-const mediaQueryIframe1440 = window.matchMedia('(max-width: 1440px)')
-const mediaQueryIframe768 = window.matchMedia('(max-width: 768px)')
-const mediaQueryIframe320 = window.matchMedia('(max-width: 320px)')
+const mediaQueryIframe1440 = window.matchMedia('(max-width: 1720px)')
+const mediaQueryIframe768 = window.matchMedia('(max-width: 1280px)')
+const mediaQueryIframe320 = window.matchMedia('(max-width: 660px)')
 if (mediaQueryIframe320.matches) {
     iframe.width = 300;
     iframe.height = 160;
@@ -19,13 +19,13 @@ if (mediaQueryIframe320.matches) {
 window.addEventListener('resize', function() {
     if (iframe) {
         let widthScreen = document.querySelector("body").offsetWidth;
-        if (widthScreen <= 320) {
+        if (widthScreen <= 660) {
             iframe.width = 300;
             iframe.height = 160;
-        } else if (widthScreen <= 768) {
+        } else if (widthScreen <= 1280) {
             iframe.width = 720;
             iframe.height = 380;
-        } else if (widthScreen <= 1440) {
+        } else if (widthScreen <= 1720) {
             iframe.width = 540;
             iframe.height = 420;
         } else {
