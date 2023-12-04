9ab3aacec0cdd416b442d70a233c18768c5d8fe6
NastyaBoiko <killer99whale@mail.ru>
2023-12-04 15:17:44 +0300

    card 320 adoptive

--- js/iframe.js
+++ js/iframe.js
@@ -5,7 +5,11 @@ console.log(iframe);
 // Создаем медиа условие, проверяющее viewports на ширину менее 1440 пикселей.
 const mediaQueryIframe1440 = window.matchMedia('(max-width: 1440px)')
 const mediaQueryIframe768 = window.matchMedia('(max-width: 768px)')
-if (mediaQueryIframe768.matches) {
+const mediaQueryIframe320 = window.matchMedia('(max-width: 320px)')
+if (mediaQueryIframe320.matches) {
+    iframe.width = 300;
+    iframe.height = 160;
+} else if (mediaQueryIframe768.matches) {
     iframe.width = 720;
     iframe.height = 380;
 } else if (mediaQueryIframe1440.matches) {
@@ -15,7 +19,10 @@ if (mediaQueryIframe768.matches) {
 window.addEventListener('resize', function() {
     if (iframe) {
         let widthScreen = document.querySelector("body").offsetWidth;
-        if (widthScreen <= 768) {
+        if (widthScreen <= 320) {
+            iframe.width = 300;
+            iframe.height = 160;
+        } else if (widthScreen <= 768) {
             iframe.width = 720;
             iframe.height = 380;
         } else if (widthScreen <= 1440) {
