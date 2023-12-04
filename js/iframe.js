f290651982832bb19ce8531a521256008e2c330d
NastyaBoiko <killer99whale@mail.ru>
2023-12-04 14:45:09 +0300

    card adoptive 768px

--- js/iframe.js
+++ js/iframe.js
@@ -3,16 +3,24 @@ console.log(iframe);
 // изменение iframe карты для экранов
 
 // Создаем медиа условие, проверяющее viewports на ширину менее 1440 пикселей.
-const mediaQueryIframe = window.matchMedia('(max-width: 1440px)')
-if (mediaQueryIframe.matches) {
+const mediaQueryIframe1440 = window.matchMedia('(max-width: 1440px)')
+const mediaQueryIframe768 = window.matchMedia('(max-width: 768px)')
+if (mediaQueryIframe768.matches) {
+    iframe.width = 720;
+    iframe.height = 380;
+} else if (mediaQueryIframe1440.matches) {
     iframe.width = 540;
 }
 // При изменении экрана
 window.addEventListener('resize', function() {
     if (iframe) {
         let widthScreen = document.querySelector("body").offsetWidth;
-        if (widthScreen <= 1440) {
+        if (widthScreen <= 768) {
+            iframe.width = 720;
+            iframe.height = 380;
+        } else if (widthScreen <= 1440) {
             iframe.width = 540;
+            iframe.height = 420;
         } else {
             iframe.width = 680;
         }
