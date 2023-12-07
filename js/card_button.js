1181a5c211a9c1ca1f76484b7c431766f2f4f497
NastyaBoiko <killer99whale@mail.ru>
2023-12-07 11:05:59 +0300

    card animations

--- js/card_button.js
+++ js/card_button.js
@@ -1,5 +1,5 @@
 descBtn = document.querySelector(".description__btn");
 console.log(descBtn);
 descBtn.addEventListener("click", function() {
-    descBtn.outerHTML = '<a href="tel:+79110367710" class="description__btn description__phone btn">+7 911-036-77-10</a>';
+    descBtn.outerHTML = '<a href="tel:+79110367710" class="description__btn description__phone btn wow animate__animated animate__pulse animate__infinite">+7 911-036-77-10</a>';
 })
