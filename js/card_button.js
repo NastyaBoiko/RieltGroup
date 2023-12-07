dff96443ae5677ea057fe898543b485c74399244
NastyaBoiko <killer99whale@mail.ru>
2023-12-07 10:58:04 +0300

    card phone js

---
+++ js/card_button.js
@@ -0,0 +1,5 @@
+descBtn = document.querySelector(".description__btn");
+console.log(descBtn);
+descBtn.addEventListener("click", function() {
+    descBtn.outerHTML = '<a href="tel:+79110367710" class="description__btn description__phone btn">+7 911-036-77-10</a>';
+})
